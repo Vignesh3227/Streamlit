246b110ec77080e8b1fbffbe083e64052a3f091c
[Vignesh Nachu] <113173309+Vignesh3227@users.noreply.github.com>
2025-03-21 12:46:19 +0530

    commit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import joblib
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
